ae6b66436c2179936c8197dceb250a27531a9e17
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-02-24 16:38:30 -0300

    Setup Initial

---
+++ project-dio.me-2025/src/test/java/dio/me/project_dio/me_2025/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.project_dio.me_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
